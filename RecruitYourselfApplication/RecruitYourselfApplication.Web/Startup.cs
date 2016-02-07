22c8f1d7e4a33964fa1ff7853fe7616216fd74a1
baretata <zlatkoatanasov92@gmail.com>
2016-02-07 23:14:56 +0200

    MVC Project introduced

---
+++ RecruitYourselfApplication/RecruitYourselfApplication.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecruitYourselfApplication.Web.Startup))]
+namespace RecruitYourselfApplication.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
