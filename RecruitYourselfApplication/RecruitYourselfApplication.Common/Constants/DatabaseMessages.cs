4b862aa6b1053851901929cff3cdfb4898b6c728
baretata <zlatkoatanasov92@gmail.com>
2016-02-07 23:15:46 +0200

    Common project introduced

---
+++ RecruitYourselfApplication/RecruitYourselfApplication.Common/Constants/DatabaseMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace RecruitYourselfApplication.Common.Constants
+{
+    public class DatabaseMessages
+    {
+        public const string DbContextNotFoundMessage = "An instance of IDbContext is required to use this repository.";
+    }
+}
