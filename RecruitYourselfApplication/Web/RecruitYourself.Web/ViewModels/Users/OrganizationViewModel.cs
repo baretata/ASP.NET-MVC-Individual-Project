9f487e1dd31209bdb80ebffad9ee29c168bbe3df
baretata <zlatkoatanasov92@gmail.com>
2016-02-17 15:19:10 +0200

    Article area added.

---
+++ RecruitYourselfApplication/Web/RecruitYourself.Web/ViewModels/Users/OrganizationViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RecruitYourself.Web.ViewModels.Users
+{
+    using System.Collections.Generic;
+    using Areas.Article.ViewModels;
+    using RecruitYourself.Web.ViewModels.Home;
+
+    public class OrganizationViewModel
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public IEnumerable<VolunteerViewModel> Members { get; set; }
+
+        public IEnumerable<EventViewModel> Events { get; set; }
+
+        public IEnumerable<ArticleViewModel> Articles { get; set; }
+    }
+}
---
+++ RecruitYourselfApplication/Web/RecruitYourself.Web/ViewModels/Users/VolunteerViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RecruitYourself.Web.ViewModels.Users
+{
+    using System.Collections.Generic;
+    using RecruitYourself.Web.ViewModels.Home;
+
+    public class VolunteerViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public short Age { get; set; }
+
+        public IEnumerable<EventViewModel> Favorites { get; set; }
+
+        public IEnumerable<EventViewModel> Volunteerships { get; set; }
+    }
+}
