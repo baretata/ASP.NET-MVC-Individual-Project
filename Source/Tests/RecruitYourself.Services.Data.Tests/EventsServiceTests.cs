0498c1fc340e25639ed01df5e88428227ddcc0e1
baretata <zlatkoatanasov92@gmail.com>
2016-02-24 01:46:38 +0200

    Added tests for the events service layer, events routing and events controllers.

---
+++ Source/Tests/RecruitYourself.Services.Data.Tests/EventsServiceTests.cs
@@ -0,0 +1,68 @@
+﻿namespace RecruitYourself.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Moq;
+    using NUnit.Framework;
+
+    using RecruitYourself.Data.Models;
+    using RecruitYourself.Services.Data.Contracts;
+
+    [TestFixture]
+    public class EventsServiceTests
+    {
+        private Mock<IEventsService> eventsServiceMock;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var eventsMock = new List<Event>().AsQueryable();
+            eventsServiceMock = new Mock<IEventsService>();
+
+            eventsServiceMock
+                .Setup(s => s.GetAll())
+                .Returns(eventsMock);
+
+            eventsServiceMock
+                .Setup(s => s.GetById("1"))
+                .Returns(new Event { Id = 1 });
+
+            eventsServiceMock
+                .Setup(s => (s.GetByPage(1, 0, 10)))
+                .Returns(eventsMock);
+
+            eventsServiceMock
+                .Setup(s => s.Add(It.IsAny<Event>()))
+                .Verifiable();
+        }
+
+        [Test]
+        public void GetAllEventsShouldNotReturnNull()
+        {
+            IQueryable<Event> articles = eventsServiceMock.Object.GetAll();
+            Assert.AreNotEqual(null, articles);
+        }
+
+        [Test]
+        public void GetByIdEventIdShouldNotReturnNull()
+        {
+            Event currentEvent = eventsServiceMock.Object.GetById("1");
+            Assert.AreEqual(1, currentEvent.Id);
+        }
+
+        [Test]
+        public void AddEventShouldBeCalled()
+        {
+            eventsServiceMock.Object.Add(new Event());
+            eventsServiceMock.Verify(s => s.Add(It.IsAny<Event>()));
+        }
+
+        [Test]
+        public void GetByPageEventsDefaultValueShouldReturnZeroIfNoEventsExist()
+        {
+            var count = eventsServiceMock.Object.GetByPage(1, 0, 10).Count();
+            Assert.AreEqual(count, 0);
+        }
+    }
+}
---
+++ Source/Tests/RecruitYourself.Web.Routes.Tests/EventsRouteTests.cs
@@ -0,0 +1,26 @@
+﻿namespace RecruitYourself.Web.Routes.Tests
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using MvcRouteTester;
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class EventsRouteTests
+    {
+        [Test]
+        public void ByIdRouteTest()
+        {
+            const string Url = "/Event/AllEvents/ById/MS4xMjMxMjMxMzEyMw==";
+            var routeCollection = new RouteCollection();
+            var areaRegistrationContext = new AreaRegistrationContext("Event", routeCollection);
+            areaRegistrationContext.MapRoute(
+                name: "EventById",
+                url: "Event/AllEvents/ById/{id}",
+                defaults: new { area = "Event", controller = "AllEvents", action = "ById", id = UrlParameter.Optional });
+
+            RouteAssert.HasRoute(routeCollection, Url, new { Area = "Event", Controller = "AllEvents", Action = "ById", Id = "MS4xMjMxMjMxMzEyMw==" });
+        }
+    }
+}
