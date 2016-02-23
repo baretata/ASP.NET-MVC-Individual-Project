13112a6d277ac02d213ecb79032575ac8b1c675d
baretata <zlatkoatanasov92@gmail.com>
2016-02-23 13:14:00 +0200

    Unit tests added for some specific routes for articles and registration.

---
+++ Source/Tests/RecruitYourself.Web.Routes.Tests/RegistrationRouteTests.cs
@@ -0,0 +1,36 @@
+﻿namespace RecruitYourself.Web.Routes.Tests
+{
+    using System.Net.Http;
+    using System.Web.Routing;
+    using Controllers;
+    using MvcRouteTester;
+    using NUnit.Framework;
+
+    using RecruitYourself.Web.Areas.Article.Controllers;
+
+    [TestFixture]
+    public class RegistrationRouteTests
+    {
+        [Test]
+        public void RegisterVolunteerRouteTest()
+        {
+            const string Url = "/Account/Register/Volunteer";
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            routeCollection
+                .ShouldMap(HttpMethod.Get, Url)
+                .To<AccountController>(c => c.Register());
+        }
+
+        [Test]
+        public void RegisterOrganizationRouteTest()
+        {
+            const string Url = "/Account/Register/Organization";
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            routeCollection
+                .ShouldMap(HttpMethod.Get, Url)
+                .To<AccountController>(c => c.Register());
+        }
+    }
+}
