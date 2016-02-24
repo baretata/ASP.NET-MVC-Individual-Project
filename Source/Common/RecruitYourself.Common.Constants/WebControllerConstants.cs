69aadea92e8005d1f16c3ced43ef94258b1b5986
baretata <zlatkoatanasov92@gmail.com>
2016-02-24 09:45:39 +0200

    Newest articles included on the home page. Caching added for the newest articles and events on the home page.

--- Source/Common/RecruitYourself.Common.Constants/WebControllerConstants.cs
+++ Source/Common/RecruitYourself.Common.Constants/WebControllerConstants.cs
@@ -3,5 +3,7 @@
     public class WebControllerConstants
     {
         public const int ItemsPerPage = 5;
+        public const int HomePageNewestEventsCount = 3;
+        public const int HomePageNewestArticlesCount = 1;
     }
 }
--- Source/Web/RecruitYourself.Web/Controllers/HomeController.cs
+++ Source/Web/RecruitYourself.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Web.Mvc;
 
+    using RecruitYourself.Common.Constants;
     using RecruitYourself.Services.Data.Contracts;
     using RecruitYourself.Web.Infrastructure.Mapping;
     using RecruitYourself.Web.ViewModels.Home;
@@ -10,23 +11,39 @@
     public class HomeController : BaseController
     {
         private readonly IEventsService events;
+        private readonly IArticlesService articles;
 
-        public HomeController(IEventsService events)
+        public HomeController(IEventsService events, IArticlesService articles)
         {
             this.events = events;
+            this.articles = articles;
         }
 
         public ActionResult Index()
         {
-            var events = this.events.GetNewestEvents(3).To<HomeEventViewModel>().ToList();
+            var newestEvents = this.Cache.Get(
+                "newestEvents",
+                () => this.events
+                    .GetNewestEvents(WebControllerConstants.HomePageNewestEventsCount)
+                    .To<EventIndexViewModel>()
+                    .ToList(),
+                30 * 60);
 
-            // var categories =
-            //    this.Cache.Get(
-            //        "categories",
-            //        () => this.categories.GetAll().To<CategoryViewModel>().ToList(),
-            //        30 * 60);
+            var newestArticles = this.Cache.Get(
+                "newestArticles",
+                () => this.articles
+                    .GetNewestEvents(WebControllerConstants.HomePageNewestArticlesCount)
+                    .To<ArticleIndexViewModel>()
+                    .ToList(),
+                30 * 60);
 
-            return this.View(events);
+            var viewModel = new IndexViewModel
+            {
+                Events = newestEvents,
+                Articles = newestArticles
+            };
+
+            return this.View(viewModel);
         }
     }
 }
