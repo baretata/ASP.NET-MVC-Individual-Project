acc202c057cca6c1aa7e2fe5dcaa7b21bc1ad55d
baretata <zlatkoatanasov92@gmail.com>
2016-02-24 14:35:55 +0200

    Error controller added to handle unauthorized users or not found views. [From the previous commit]Added global error handling method to redirect to NotFoundPage view if requested url does not meet any route mappings.

---
+++ Source/Web/RecruitYourself.Web/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿namespace RecruitYourself.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : BaseController
+    {
+        public ActionResult Unauthorized()
+        {
+            return this.View();
+        }
+
+        public ActionResult PageNotFound()
+        {
+            return this.View();
+        }
+    }
+}
